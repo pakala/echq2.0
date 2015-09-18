ae3266536067ce02cdabf371f8de3f4e5566cd1c
pakala <shashi.pakala@gmail.com>
2015-09-18 14:48:31 +1000

    Used delimiter method,updated styling

--- app/models/cheque.rb
+++ app/models/cheque.rb
@@ -3,5 +3,5 @@ class Cheque < ActiveRecord::Base
 
   validates :amount, :presence => true
   validates :recipient_id, :presence => {:message => " needs to be selected"}
-  validates :amount, numericality: {only_decimal: true, greater_than_or_equal_to: 1}
+  validates :amount, numericality: {only_decimal: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 999999.99}
 end
